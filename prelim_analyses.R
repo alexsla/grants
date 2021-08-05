a830d006207de07f77d68b40bcedefe4887a8f5d
Alex Slavenko <44232223+alexsla@users.noreply.github.com>
2021-08-05 13:45:19 +0300

    new plots

--- prelim_analyses.R
+++ prelim_analyses.R
@@ -38,6 +38,20 @@ dat %>%
   theme(legend.position = "none")
 dev.off()
 
+pdf("Plots/ecology_by_time.pdf", useDingbats = F)
+dat %>%
+  group_by(Year, Field) %>%
+  summarise(n = n()) %>%
+  mutate(freq = n / sum(n)) %>%
+  drop_na() %>%
+  filter(Field == "Ecology") %>%
+  ggplot(aes(x = as.numeric(Year),
+             y = freq)) +
+  geom_point() +
+  geom_smooth(method = "loess") +
+  theme(legend.position = "none")
+dev.off()
+
 pdf("Plots/field_freq.pdf", useDingbats = F)
 dat %>%
   mutate(Field = recode(Field,
@@ -48,14 +62,13 @@ dat %>%
   mutate(freq = n / sum(n)) %>%
   drop_na() %>%
   filter(Field %in% fields) %>%
-  ggplot(aes(x = Field,
+  ggplot(aes(x = reorder(Field, -freq, na.rm = TRUE),
              y = freq,
              fill = Field)) +
   geom_boxplot() +
   theme(legend.position = "none",
         axis.text.x = element_text(angle = 45, hjust = 1))
 dev.off()
-
 
 pdf("Plots/field_funding_by_time.pdf", useDingbats = F)
 dat %>%
@@ -83,10 +96,44 @@ dat %>%
   group_by(Year, Field) %>%
   drop_na() %>%
   filter(Field %in% fields) %>%
-  ggplot(aes(x = Field,
+  ggplot(aes(x = reorder(Field, -Per_annum_budget, na.rm = TRUE),
              y = Per_annum_budget,
              fill = Field)) +
   geom_boxplot() +
   theme(legend.position = "none",
         axis.text.x = element_text(angle = 45, hjust = 1))
 dev.off()
+
+pdf("Plots/institution_freq_ecology.pdf", useDingbats = F)
+dat %>%
+  group_by(Year, Field, Institution) %>%
+  summarise(n = n()) %>%
+  mutate(freq = n / sum(n)) %>%
+  drop_na() %>%
+  filter(Field == "Ecology") %>%
+  ggplot(aes(x = reorder(Institution, -freq, na.rm = TRUE),
+             y = freq,
+             fill = Institution)) +
+  geom_boxplot() +
+  theme(legend.position = "none",
+        axis.text.x = element_text(angle = 45, hjust = 1))
+dev.o
+
+pdf("Plots/institution_freq_ecology_by_time.pdf", useDingbats = F, width = 15)
+dat %>%
+  filter(Field == "Ecology") %>%
+  group_by(Year, Institution) %>%
+  summarise(n = n()) %>%
+  mutate(freq = n / sum(n)) %>%
+  drop_na() %>%
+  ggplot(aes(x = as.numeric(Year),
+             y = freq,
+             fill = Institution,
+             colour = Institution)) +
+  geom_point() +
+  geom_smooth(method = "loess",
+              span = 1,
+              se = F) +
+  theme(legend.position = "bottom")
+dev.off()
+
